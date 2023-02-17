f2f8c238357979076c29ec03933542143b4792b1
Oleksandr Rybalka <alexrybalka65@gmail.com>
2023-02-17 19:29:26 +0200

    загружает стартовые файлы

---
+++ src/js/task-01.js
@@ -0,0 +1,7 @@
+const numberOfCategories = document.querySelectorAll('.item');
+console.log(`Number of categories:${numberOfCategories.length}`);
+
+const text = document.querySelectorAll('.h2');
+console.log(text);
+
+// const list = body.firstElementChild;
