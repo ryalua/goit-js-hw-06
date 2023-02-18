250060c82358e5356d9a077eb3d777afa6c4efb6
Oleksandr Rybalka <alexrybalka65@gmail.com>
2023-02-18 21:17:25 +0200

    Done task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector('#ingredients');
+// console.log(list);
+const ingredientsRef = ingredients.map(ingredient => {
+  const liRef = document.createElement('li');
+  liRef.textContent = ingredient;
+  liRef.classList.add('item');
+
+  // console.log(liRef);
+  return liRef;
+  
+});
+list.append(...ingredientsRef);
